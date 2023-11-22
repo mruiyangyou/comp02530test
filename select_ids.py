f55ba22d5a6dbbef7fc1519bd4b1f6a029efa84a
mruiyangyou <m.ruiyangyou@gmail.com>
2023-11-22 11:47:49 +0000

    add seperate task file

--- select_ids.py
+++ select_ids.py
@@ -1,25 +1,45 @@
 import sys
 import random
 from Bio import SeqIO
+from typing import List
 
 """
 usage: python select_ids.py INPUT.fasta 2000
 """
 
-def read_input(file):
+def read_ids(file: str) -> List[str]:
+    res: List[str] = []
+    with open(file, 'r') as f:
+        for line in f:
+            res.append(line.strip())
+    return res
+
+def match_ids(file: str, ids: List[str]) -> List[str]:
     """
     Function reads a fasta formatted file of protein sequences
     """
     # print("READING FASTA FILES")
-    ids = []
+    records = []
     for record in SeqIO.parse(file, "fasta"):
-        ids.append(record.id)
-    return(ids)
+        if record.id in ids:
+            records.append(record)
+    return records
+
+def write_fasta(records: List, number: int) -> None:
+    """
+    Function writes the list of SeqIO records to a fasta formatted file.
+    """
+    split_records: List[List[str]] = [records[i::number] for i in range(number)]
+    for i, records in enumerate(split_records):
+        with open(f'data/exp/experiemnt_{i}.fasta', "w") as output_handle:
+            SeqIO.write(records, output_handle, "fasta")
 
 
 if __name__ == "__main__":
-    ids = read_input(sys.argv[1])
-    # print(ids)
-    rand_list = random.sample(ids, int(sys.argv[2]))
-    for id in rand_list:
-        print(id)
+    ids = read_ids(sys.argv[1])
+    res = match_ids(sys.argv[2], ids)
+    if len(res) == len(ids):
+        print('All ids are matched!')
+    else:
+        print(f'{len(res)} ids are matched!')
+    write_fasta(res, int(sys.argv[3]))
